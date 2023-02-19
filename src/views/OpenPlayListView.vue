970e2f7c8cef55ce45ae8440128c8e9b26e66453
jsb8740 <jsb87400@gmail.com>
2023-02-20 02:32:04 +0900

    플레이리스트 페이지 추가

---
+++ src/views/OpenPlayListView.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <div>
+      <h1>저장한 플레이리스트</h1>
+      <div>플레이리스트들</div>
+    </div>
+
+    <!-- 컴포넌트화 해야할듯 -->
+
+    <hr />
+
+    <div>
+      <h1>공개된 플레이리스트</h1>
+      <div>플레이리스트들</div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
