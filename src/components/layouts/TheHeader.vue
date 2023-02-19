eeffa0fbbe0e4827936a5e7bb7b47ba18b4d706a
jsb8740 <jsb87400@gmail.com>
2023-02-20 02:30:12 +0900

    레이아웃 추가

---
+++ src/components/layouts/TheHeader.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="header">
+    <RouterLink :to="{ name: 'Home' }">
+      <div>홈</div>
+    </RouterLink>
+    <RouterLink :to="{ name: 'OpenPlayList' }">
+      <div>플레이리스트</div>
+    </RouterLink>
+
+    <div>TOP100</div>
+    <div>
+      <input type="text" />
+      검색
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped lang="scss">
+.header {
+  display: flex;
+  gap: 30px;
+  justify-content: center;
+  font-size: x-large;
+  position: sticky;
+  top: 8px;
+}
+</style>
