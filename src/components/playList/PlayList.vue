c997c65558012ada2fd1716291ba427cc510ffed
qwer1234 <qwer1234@jinjoosoft.com>
2023-02-21 21:56:35 +0900

    컴포넌트 추가

---
+++ src/components/playList/PlayList.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts"></script>
+<template>
+  <div class="playList">
+    <slot />
+  </div>
+</template>
+
+<style scoped lang="scss">
+.playList {
+  border: 1px solid #afc4e7;
+  padding: 20px 50px;
+}
+</style>
---
+++ src/components/playList/PlayListItem.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <img src="@/assets/test/test1.jpg" />
+    <div>
+      <slot name="title">title</slot>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
