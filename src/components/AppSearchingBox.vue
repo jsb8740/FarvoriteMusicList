3ab41a09e2b75efdff120f819ba3694f09d2cc01
jsb8740 <jsb87400@gmail.com>
2023-02-23 03:04:05 +0900

    serchingBox 컴포넌트 생성

---
+++ src/components/AppSearchingBox.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="searchingBox" @click="visible = !visible">
+    <Search></Search>
+
+    <!-- input은 이벤트 버블링인가 그걸 막아야할듯 -->
+    <!-- transition 애니메이션 넣기 -->
+    <input type="text" :hidden="visible" />
+
+    <div>검색</div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+import Search from "./icons/Search.vue";
+
+const visible = ref(true);
+</script>
+
+<style scoped lang="scss">
+.searchingBox {
+  cursor: pointer;
+  display: flex;
+
+  align-items: center;
+  &:hover {
+  }
+}
+</style>
