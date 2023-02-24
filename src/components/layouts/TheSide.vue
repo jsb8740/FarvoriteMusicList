d4d359575c0c1e07cb7f5c9f16bcf9b7dac4296d
qwer1234 <qwer1234@jinjoosoft.com>
2023-02-24 22:14:28 +0900

    layout 변경 header -> side

---
+++ src/components/layouts/TheSide.vue
@@ -0,0 +1,49 @@
+<template>
+  <aside class="header" role="navigation">
+    <img src="@/assets/test/Logo-Test.png" alt="" />
+
+    <div>
+      <div class="category">Menu</div>
+
+      <RouterLink :to="{ name: 'Home' }">
+        <div>홈</div>
+      </RouterLink>
+
+      <div>카테고리</div>
+
+      <div>TOP100</div>
+    </div>
+
+    <div>
+      <div class="category">Playlists</div>
+
+      <RouterLink :to="{ name: 'OpenPlayList' }">
+        <div>공개된 플레이리스트</div>
+      </RouterLink>
+    </div>
+  </aside>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped lang="scss">
+.header {
+  display: flex;
+  flex-direction: column;
+
+  max-width: 12rem;
+
+  gap: 2rem;
+  font-size: 1.4rem;
+  position: sticky;
+  top: 0px;
+
+  background-color: rgb(242, 241, 246);
+  padding: 0 2.4rem;
+
+  .category {
+    color: rgb(164, 164, 174);
+    margin: 1em 0;
+  }
+}
+</style>
