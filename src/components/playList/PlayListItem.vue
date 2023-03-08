6be600cfa18ce2c9e3dac90155199e2c98c65b78
qwer1234 <qwer1234@jinjoosoft.com>
2023-03-08 19:19:54 +0900

    title byWriter 추가

--- src/components/playList/PlayListItem.vue
+++ src/components/playList/PlayListItem.vue
@@ -4,8 +4,13 @@
       <img src="@/assets/test/test1.jpg" class="" />
       <Heart class="favorite" />
     </div>
-    <div>
+
+    <div class="title">
       <slot name="title">title</slot>
+    </div>
+
+    <div class="byWriter">
+      <slot name="by">by test</slot>
     </div>
   </div>
 </template>
@@ -18,11 +23,20 @@ import Heart from "@/components/icons/Heart.vue";
 .listItem {
   margin: 0 2rem;
   display: inline-block;
+  .byWriter {
+    margin-top: 0.3rem;
+    color: rgb(158, 158, 159);
+  }
+
+  .title {
+    margin-top: 0.3rem;
+  }
+
   .thumbnail {
     position: relative;
     img {
-      width: 200px;
-      height: 200px;
+      width: 250px;
+      height: 250px;
 
       border-radius: 1rem;
     }
