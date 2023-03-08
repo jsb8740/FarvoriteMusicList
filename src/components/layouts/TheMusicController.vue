8e819d4927e5d72f67617c812ae8f1c77205c75d
qwer1234 <qwer1234@jinjoosoft.com>
2023-03-08 19:18:45 +0900

    scss 바꿈

--- src/components/layouts/TheMusicController.vue
+++ src/components/layouts/TheMusicController.vue
@@ -1,16 +1,51 @@
 <template>
   <div class="controller">
-    <img src="@/assets/test/test1.jpg" alt="" />
+    <div class="progress">
+      <svg></svg>
+    </div>
+
+    <div class="MusicPlayer">
+      <img src="@/assets/test/test1.jpg" alt="" />
+
+      <div class="test">ff</div>
+      <div class="soundControl">
+        <Heart></Heart>
+      </div>
+    </div>
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import Heart from "../icons/Heart.vue";
+</script>
 
 <style scoped lang="scss">
+@import "@/assets/color.scss";
 .controller {
   background-color: white;
   // height: 6.5rem;
-  padding-right: 3rem;
+  // padding-right: 3rem;
+
+  .MusicPlayer {
+    display: flex;
+    align-items: center;
+    .test {
+      display: flex;
+      width: 100%;
+      justify-content: center;
+      background-color: greenyellow;
+    }
+  }
+
+  .progress {
+    background-color: rgb(222, 222, 222);
+    svg {
+      display: block;
+      background-color: $orangeColor;
+      height: 7px;
+      width: 75%;
+    }
+  }
 
   img {
     width: 6.5rem;
