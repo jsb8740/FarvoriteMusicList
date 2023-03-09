924a0d3fe111dc1e30b0de66e1943cc9a301aa7f
jsb8740 <jsb87400@gmail.com>
2023-03-10 03:23:38 +0900

    클릭으로 음량조절 추가

--- src/components/music-controller/SoundController.vue
+++ src/components/music-controller/SoundController.vue
@@ -1,16 +1,18 @@
 <template>
   <div>
-    <div class="iconView" @click="soundMute">
-      <img v-show="true" src="@/assets/volumeMute-white.png" />
-      <img v-show="false" src="@/assets/volumeLow-white.png" />
-      <img v-show="false" src="@/assets/volumeHigh-white.png" />
-    </div>
+    <button class="iconView" @click="soundMute">
+      <img v-show="soundMuteImg" src="@/assets/volumeMute-white.png" />
+      <img v-show="soundLowImg" src="@/assets/volumeLow-white.png" />
+      <img v-show="soundHighImg" src="@/assets/volumeHigh-white.png" />
+    </button>
     <div class="soundWrap">
       <div
         class="soundBar"
+        @drag="volumeDrag"
+        @click="volumeClick"
         @wheel="volumeHandler"
         :style="{ '--soundWidth': dynamicWidth }"
-      />
+      ></div>
     </div>
   </div>
 </template>
@@ -21,18 +23,22 @@ import { ref, computed } from "vue";
 const volume = ref(100);
 
 const dynamicWidth = computed(() => `${volume.value}%`);
-const dynamicAudioImg = computed(() => {
-  if (volume.value === 0) {
-    //사운드가 뮤트인경우
-  } else if (volume.value <= 50) {
-    //50보다 작거나 같으면
-  } else {
-    //50보다크면
-  }
-});
+
+// sound img 컨트롤
+const soundMuteImg = computed<boolean>(() =>
+  volume.value === 0 ? true : false
+);
+const soundLowImg = computed<boolean>(() =>
+  volume.value > 0 && volume.value <= 50 ? true : false
+);
+const soundHighImg = computed<boolean>(() =>
+  volume.value > 50 && volume.value <= 100 ? true : false
+);
 
 const muteFlag = ref(false); //뮤트 확인  플래그
 const volumeTmp = ref(0); // 뮤트인 경우 볼륨을 담는 임시변수 뮤트끝나면 복귀시키는 용도
+
+// 사운드 이미지 클릭시 소리 mute
 const soundMute = () => {
   muteFlag.value = !muteFlag.value;
   if (muteFlag.value) {
@@ -46,12 +52,14 @@ const soundMute = () => {
   }
 };
 
+// 볼륨 핸들러
 const volumeHandler = (e: WheelEvent) => {
   volume.value += volumeUpDown(e.deltaY);
   volumeLimit();
   console.log("현재볼륨", volume.value);
 };
 
+//볼륨 0, 100 밖으로 안벗어나게
 const volumeLimit = () => {
   if (volume.value < 0) {
     volume.value = 0;
@@ -60,18 +68,41 @@ const volumeLimit = () => {
   }
 };
 
+// 마우스 휠 이벤트 컨트롤
 const volumeUpDown = (upDown: number): number => {
-  if (upDown < 0) {
-    //up
-    console.log("볼륨 업", volume.value);
-    return 5;
-  } else if (upDown > 0) {
-    //down
-    console.log("볼륩 다운", volume.value);
-    return -5;
-  }
+  if (muteFlag.value === false) {
+    // 뮤트가 아닌 경우
 
+    if (upDown < 0) {
+      //up
+      console.log("볼륨 업", volume.value);
+      return 5;
+    } else if (upDown > 0) {
+      //down
+      console.log("볼륩 다운", volume.value);
+      return -5;
+    }
+  }
+  // 뮤트인 경우
   return 0;
+};
+
+const volumeClick = (e: MouseEvent) => {
+  // console.log("click", e);
+  // 마우스 클릭
+  muteFlag.value = false;
+  volumeTmp.value = 0;
+  volume.value = Math.round((e.offsetX / 200) * 100);
+};
+
+const volumeDrag = (e: DragEvent) => {
+  console.log("drag", e.offsetX);
+  muteFlag.value = false;
+  volumeTmp.value = 0;
+  volume.value = Math.round((e.offsetX / 200) * 100);
+
+  // 할것 드래그하면 이미지 사라지는것 고치기
+  // 드래그 할때 pointer로 교체
 };
 </script>
 
@@ -79,10 +110,13 @@ const volumeUpDown = (upDown: number): number => {
 .iconView {
   position: relative;
   height: 2rem;
+  width: 2rem;
   img {
+    inset: 0;
     position: absolute;
     height: 100%;
     cursor: pointer;
+    transition: 0.3s;
   }
 }
 .soundWrap {
@@ -96,6 +130,7 @@ const volumeUpDown = (upDown: number): number => {
     background-color: gray;
     width: 100%;
     height: 100%;
+
     &::before {
       content: "";
       position: absolute;
