4bc249dc5d8106325f385e8c9d816d6c1561221f
jsb8740 <jsb87400@gmail.com>
2023-03-13 03:21:45 +0900

    favorites의 music item 컴포넌트

---
+++ src/components/playList/musicList/MusicItem.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="row">
+    <slot name="order">#1</slot>
+    <slot name="thumbnail">
+      <img src="@/assets/test/test1.jpg" alt="test" srcset="" />
+    </slot>
+
+    <slot name="title">title</slot>
+    <slot name="time">time</slot>
+    <slot name="heart">Heart</slot>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped lang="scss">
+.row {
+  height: 3rem;
+  display: flex;
+  align-items: center;
+  padding: 0.7rem 0;
+  img {
+    height: 100%;
+    width: auto;
+    border-radius: 0.3rem;
+  }
+}
+</style>
