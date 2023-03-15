ec3aaadaa04608a89cc69e1f2b1d013030cbdc44
qwer1234 <qwer1234@jinjoosoft.com>
2023-03-15 19:25:53 +0900

    layout 구조 교체

--- src/components/layouts/TheViewLayout.vue
+++ src/components/layouts/TheViewLayout.vue
@@ -1,9 +1,5 @@
 <template>
   <div class="contentLayout">
-    <div class="searching">
-      <AppSearchingBox></AppSearchingBox>
-      <AppDarkMode></AppDarkMode>
-    </div>
     <slot></slot>
   </div>
 </template>
@@ -13,15 +9,4 @@ import AppDarkMode from "../AppDarkMode.vue";
 import AppSearchingBox from "../AppSearchingBox.vue";
 </script>
 
-<style scoped lang="scss">
-.contentLayout {
-  padding: 2rem 3rem;
-  //   height: 100%;
-}
-.searching {
-  display: flex;
-  justify-content: space-between;
-  margin-bottom: 3rem;
-  //
-}
-</style>
+<style scoped lang="scss"></style>
