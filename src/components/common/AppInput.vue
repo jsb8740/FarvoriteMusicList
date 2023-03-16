a5d4ea15801e2e71306a6fcbbe28780e690a9cab
qwer1234 <qwer1234@jinjoosoft.com>
2023-03-16 19:05:58 +0900

    input text를 따로 빼기

---
+++ src/components/common/AppInput.vue
@@ -0,0 +1,26 @@
+<template>
+  <input :value="value" @input="updateValue" />
+</template>
+
+<script setup lang="ts">
+import { computed } from "vue";
+
+export interface Props {
+  modelValue?: unknown;
+}
+
+interface Emits {
+  (e: "update:modelValue", value: Props["modelValue"]): void;
+}
+
+const props = defineProps<Props>();
+const emit = defineEmits<Emits>();
+
+const value = computed<Props["modelValue"]>(() => props.modelValue);
+
+const updateValue = (event: Event) => {
+  emit("update:modelValue", (event.target as HTMLInputElement).value);
+};
+</script>
+
+<style scoped lang="scss"></style>
