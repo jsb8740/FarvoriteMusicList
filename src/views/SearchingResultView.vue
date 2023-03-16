90f4d6cc7bcff8142aa48ec22ed5d2ebeedd4ec3
jsb8740 <jsb87400@gmail.com>
2023-03-17 02:35:14 +0900

    Musicitem 폐기 playListitem으로 변겨

--- src/views/SearchingResultView.vue
+++ src/views/SearchingResultView.vue
@@ -3,18 +3,35 @@
     <template #default>
       <!-- <img src="https://i.ytimg.com/vi/U5TTMeIadME/default.jpg" /> -->
       <!-- {{ searchResult }} -->
-      <MusicItem v-for="i in searchResult?.data.items.length">
-        <template #order>
-          {{ i }}
+      <PlayListItem v-for="(item, index) in searchResult?.data.items">
+        <template #thumbnail>
+          <img :src="item.snippet.thumbnails.default.url" />
         </template>
-      </MusicItem>
+        <template #title>
+          {{ item.snippet.title }}
+        </template>
+      </PlayListItem>
+      <!-- playList로 교체 item마다 id값을 props로 받아서 저장 -->
+      <!-- 유튜브 할당량이 정해져있어서 어쩔 수업슴 -->
+      <!-- 시간도 뮤직 컨트롤로에만 있게 -->
+      <!-- 한줄에 3개씩? 4개씩 -->
+      <!-- 무한 스크롤 -->
+
+      <!-- <PlayListItem v-for="(item, index) in searchResult?.data.items">
+        <template #thumbnail>
+          <img :src="item.snippet.thumbnails.default.url" />
+        </template>
+        <template #title>
+          {{ item.snippet.title }}
+        </template>
+      </PlayListItem> -->
     </template>
   </TheViewLayout>
 </template>
 
 <script setup lang="ts">
 import TheViewLayout from "@/components/layouts/TheViewLayout.vue";
-import MusicItem from "@/components/playList/musicList/MusicItem.vue";
+import PlayListItem from "@/components/playList/PlayListItem.vue";
 import { useSearchStore } from "@/stores/search";
 import { storeToRefs } from "pinia";
 import { useRoute } from "vue-router";
