2c5de4f6ba41a9f9b20a2616269ca61d2affa156
jsb8740 <jsb87400@gmail.com>
2023-04-03 01:16:50 +0900

    좋아요한 노래 기능 추가

--- src/views/FavoriteMusicListView.vue
+++ src/views/FavoriteMusicListView.vue
@@ -3,8 +3,13 @@
     <template #default>
       <h2>Favorite Musics</h2>
       <div>
-        <MusicItem></MusicItem>
-        <MusicItem></MusicItem>
+        <!-- <MusicItem></MusicItem>
+        <MusicItem></MusicItem> -->
+        <PlayListItem v-for="item in favSongList" :video-id="item.videoId">
+        </PlayListItem>
+        <!-- <div v-for="item in favSongList">
+            {{ item.videoId }}
+          </div> -->
       </div>
 
       <!-- Music Itme을 2초 정도 누르면 드래그앤 드롭이 가능하게끔 -->
@@ -14,7 +19,18 @@
 
 <script setup lang="ts">
 import TheViewLayout from "@/components/layouts/TheViewLayout.vue";
-import MusicItem from "@/components/playList/musicList/MusicItem.vue";
+import PlayListItem from "@/components/playList/PlayListItem.vue";
+import { onMounted } from "vue";
+import { storeToRefs } from "pinia";
+import { useIndexedDBStore } from "@/stores/indexedDB";
+
+const store = useIndexedDBStore();
+const { favSongList } = storeToRefs(store);
+
+onMounted(() => {
+  store.getFavList();
+  // console.log(favSongList.value);
+});
 </script>
 
-<style scoped></style>
+<style scoped lang="scss"></style>
