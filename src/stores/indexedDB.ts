23ff1d078519fa60ab12c015c265679154107d71
qwer1234 <qwer1234@jinjoosoft.com>
2023-04-04 23:22:11 +0900

    pinia indexedDB 추가 삭제 추가

--- src/stores/indexedDB.ts
+++ src/stores/indexedDB.ts
@@ -6,10 +6,22 @@ export const useIndexedDBStore = defineStore("indexedDB", () => {
   const favSongList = ref();
   const webDB = DataBase.getInstance();
 
-  async function getFavList() {
+  async function updateFavList() {
     favSongList.value = await webDB.getFavList();
-    console.log(favSongList.value);
+    // console.log(favSongList.value);
   }
 
-  return { getFavList, favSongList };
+  async function addSong(videoId: string, title: string) {
+    webDB.addData(videoId, title);
+    // favSongList.value = await webDB.getFavList();
+    await updateFavList();
+    console.log("add", favSongList);
+  }
+
+  async function removeSong(videoId: string) {
+    webDB.deleteData(videoId);
+    // favSongList.value = await webDB.getFavList();
+    // await updateFavList();
+  }
+  return { updateFavList, removeSong, addSong, favSongList };
 });
