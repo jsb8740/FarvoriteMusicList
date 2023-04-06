25c03a0400c52c3f6d98af58e237bedbfe1978da
qwer1234 <qwer1234@jinjoosoft.com>
2023-04-06 21:33:35 +0900

    soundController 컴포넌트 pinia 연결

--- src/components/musicController/SoundController.vue
+++ src/components/musicController/SoundController.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="soundController">
-    <button class="iconView" @click="soundMute">
+    <button class="iconView" @click="store.soundMute">
       <img v-show="soundMuteImg" src="@/assets/volumeMute-white.png" />
       <img v-show="soundLowImg" src="@/assets/volumeLow-white.png" />
       <img v-show="soundHighImg" src="@/assets/volumeHigh-white.png" />
@@ -21,26 +21,20 @@
 
 <script setup lang="ts">
 import { ref, computed, onMounted } from "vue";
+import { useMusicControllerStore } from "@/stores/musicController";
+import { storeToRefs } from "pinia";
 
 enum VolumeControllerState {
   IDLE,
   DRAGGING,
 }
 
-const volume = ref(100);
+const store = useMusicControllerStore();
+const { volume } = storeToRefs(store);
 
 onMounted(() => {
-  volumeInit();
+  store.volumeInit();
 });
-const volumeInit = (): void => {
-  if (localStorage.getItem("volume") === null) {
-    volume.value = 100;
-    return;
-  }
-
-  volume.value = parseInt(localStorage.getItem("volume") as string);
-  return;
-};
 
 const volumeControllerState = ref<VolumeControllerState>(
   VolumeControllerState.IDLE
@@ -59,57 +53,9 @@ const soundHighImg = computed<boolean>(() =>
   volume.value > 50 && volume.value <= 100 ? true : false
 );
 
-const muteFlag = ref(false); //뮤트 확인  플래그
-const volumeTmp = ref(0); // 뮤트인 경우 볼륨을 담는 임시변수 뮤트끝나면 복귀시키는 용도
-
-// 사운드 이미지 클릭시 소리 mute
-const soundMute = () => {
-  muteFlag.value = !muteFlag.value;
-  if (muteFlag.value) {
-    // 뮤트인 경우
-    volumeTmp.value = volume.value;
-    volume.value = 0;
-  } else {
-    //뮤트 아닌 경우
-    volume.value = volumeTmp.value;
-    volumeTmp.value = 0;
-  }
-};
-
 // 볼륨 핸들러
 const onMouseWheel = (e: WheelEvent) => {
-  volume.value += volumeUpDown(e.deltaY);
-  volumeLimit();
-  localStorage.setItem("volume", volume.value.toString());
-  // console.log("현재볼륨", volume.value);
-};
-
-//볼륨 0, 100 밖으로 안벗어나게
-const volumeLimit = () => {
-  if (volume.value < 0) {
-    volume.value = 0;
-  } else if (volume.value > 100) {
-    volume.value = 100;
-  }
-};
-
-// 마우스 휠 이벤트 컨트롤
-const volumeUpDown = (upDown: number): number => {
-  if (muteFlag.value === false) {
-    // 뮤트가 아닌 경우
-
-    if (upDown < 0) {
-      //up
-      // console.log("볼륨 업", volume.value);
-      return 5;
-    } else if (upDown > 0) {
-      //down
-      // console.log("볼륩 다운", volume.value);
-      return -5;
-    }
-  }
-  // 뮤트인 경우
-  return 0;
+  store.volumeUpDownHandler(e.deltaY);
 };
 
 const onMouseClick = (e: MouseEvent) => {
@@ -119,7 +65,7 @@ const onMouseClick = (e: MouseEvent) => {
   }
 
   // 클릭시 마우스 체크
-  updateVolume(e.offsetX);
+  store.updateVolume(e.offsetX);
 };
 
 const onMouseRelease = (e: MouseEvent) => {
@@ -130,20 +76,9 @@ const onMouseRelease = (e: MouseEvent) => {
 
 const onMouseMove = (e: MouseEvent) => {
   //mouseMove 상태이고 드래그인 상태
-  // console.log(e.offsetY);
-
   if (volumeControllerState.value === VolumeControllerState.DRAGGING) {
-    updateVolume(e.offsetX);
+    store.updateVolume(e.offsetX);
   }
-};
-
-const updateVolume = (offsetX: number) => {
-  muteFlag.value = false;
-  volumeTmp.value = 0;
-  volume.value = Math.round((offsetX / 200) * 100);
-  console.log(typeof volume.value);
-
-  localStorage.setItem("volume", volume.value.toString());
 };
 </script>
 
