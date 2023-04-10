be57d8db3f78629201ae5c87e56deec134ef4de9
jsb8740 <jsb87400@gmail.com>
2023-04-11 02:33:18 +0900

    재사용을 위해서 soundController.vue에서 분리함

---
+++ src/components/common/AppProgressBar.vue
@@ -0,0 +1,121 @@
+<template>
+  <div class="progressWrap" @wheel.prevent.stop>
+    <div
+      class="progressBar"
+      @mousedown="onMouseClick"
+      @mouseup="onMouseRelease"
+      @mousemove="onMouseMove"
+      @wheel="onMouseWheel"
+      :style="{ '--progressWidth': typeWidth }"
+    ></div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { useMusicControllerStore } from "@/stores/musicController";
+import { useSoundControllerStore } from "@/stores/soundController.js";
+import { storeToRefs } from "pinia";
+import { computed, ref } from "vue";
+
+export interface Props {
+  type: string;
+}
+
+enum VolumeControllerState {
+  IDLE,
+  DRAGGING,
+}
+
+// soundStore
+const soundStore = useSoundControllerStore();
+const musicPlayStore = useMusicControllerStore();
+
+const props = defineProps<Props>();
+const volumeControllerState = ref<VolumeControllerState>(
+  VolumeControllerState.IDLE
+);
+
+const typeWidth = computed(() => {
+  if (props.type === "sound") {
+    return soundStore.dynamicSoundWidth;
+  } else {
+    return musicPlayStore.dynamicMusicdWidth;
+  }
+});
+
+// videostore
+
+// 볼륨 핸들러
+const onMouseWheel = (e: WheelEvent) => {
+  if (props.type === "sound") {
+    soundStore.volumeUpDownHandler(e.deltaY);
+  }
+};
+
+const onMouseClick = (e: MouseEvent) => {
+  // 클릭시 볼륨 컨트롤 상태 -> 드래그
+  if (volumeControllerState.value === VolumeControllerState.IDLE) {
+    volumeControllerState.value = VolumeControllerState.DRAGGING;
+  }
+
+  // 클릭시 마우스 체크
+  if (props.type === "sound") {
+    soundStore.updateVolume(e.offsetX);
+  } else {
+    musicPlayStore.updateTime(e.offsetX);
+  }
+};
+
+const onMouseRelease = (e: MouseEvent) => {
+  if (volumeControllerState.value === VolumeControllerState.DRAGGING) {
+    volumeControllerState.value = VolumeControllerState.IDLE;
+  }
+};
+
+const onMouseMove = (e: MouseEvent) => {
+  //mouseMove 상태이고 드래그인 상태
+  if (volumeControllerState.value === VolumeControllerState.DRAGGING) {
+    if (props.type === "sound") {
+      soundStore.updateVolume(e.offsetX);
+    } else {
+      musicPlayStore.updateTime(e.offsetX);
+    }
+  }
+};
+</script>
+
+<style scoped lang="scss">
+@import "@/assets/color.scss";
+
+.soundController {
+  display: flex;
+  align-items: center;
+  gap: 0.5rem;
+  padding: 0 1rem;
+}
+
+.progressWrap {
+  overflow: hidden;
+  position: relative;
+  width: 200px;
+  height: 12px;
+  cursor: pointer;
+
+  $radius: 0.5rem;
+  .progressBar {
+    background-color: $progressBackColor;
+    width: 100%;
+    height: 100%;
+    border-radius: $radius;
+
+    &::before {
+      content: "";
+      position: absolute;
+      background-color: $orangeColor;
+      width: var(--progressWidth);
+      height: 100%;
+      border-radius: $radius;
+    }
+  }
+}
+</style>
