4417fa8237786b1387a9f3e30fb62f5c895ab7fd
qwer1234 <qwer1234@jinjoosoft.com>
2023-04-10 20:35:21 +0900

    주석 삭제

--- src/stores/indexedDB.ts
+++ src/stores/indexedDB.ts
@@ -8,20 +8,15 @@ export const useIndexedDBStore = defineStore("indexedDB", () => {
 
   async function updateFavList() {
     favSongList.value = await webDB.getFavList();
-    // console.log(favSongList.value);
   }
 
   async function addSong(videoId: string, title: string) {
     webDB.addData(videoId, title);
-    // favSongList.value = await webDB.getFavList();
     await updateFavList();
-    console.log("add", favSongList);
   }
 
   async function removeSong(videoId: string) {
     webDB.deleteData(videoId);
-    // favSongList.value = await webDB.getFavList();
-    // await updateFavList();
   }
   return { updateFavList, removeSong, addSong, favSongList };
 });
