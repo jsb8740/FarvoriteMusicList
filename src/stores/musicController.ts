053bdfe4c6464fa89bee8bddcbb383a59137e720
qwer1234 <qwer1234@jinjoosoft.com>
2023-04-11 19:24:21 +0900

    inputPlaylist랑 shuffle 기능 추가

--- src/stores/musicController.ts
+++ src/stores/musicController.ts
@@ -1,15 +1,45 @@
-import { computed, ref } from "vue";
+import { computed, ref, toRef, toRefs } from "vue";
 import { defineStore } from "pinia";
+import { useIndexedDBStore } from "./indexedDB";
 
 export const useMusicControllerStore = defineStore("play", () => {
   const currentTime = ref(0);
   const duration = ref(0);
   const isPaused = ref(true); // 음악 시작 state
   const clickedTime = ref(0);
+  const playList = ref<string[]>([]);
+  const indexedDBStore = useIndexedDBStore();
 
   const dynamicMusicdWidth = computed(
     () => `${Math.round((currentTime.value / duration.value) * 100)}%`
   );
+
+  const inputPlayList = async () => {
+    playList.value = await indexedDBStore.getPlayList;
+    console.log(playList.value);
+  };
+  const shufflePlaylist = () => {
+    if (playList.value.length === 0) return;
+
+    // 원본 복사
+    const originalArray: string[] = Object.values(playList.value);
+
+    const shuffleArray: string[] = [];
+
+    // 피셔 예이츠 셔플
+    while (true) {
+      if (originalArray.length === 0) {
+        break;
+      }
+      const randomNumberIndex = Math.floor(
+        Math.random() * originalArray.length
+      );
+      const sliceValue = originalArray.splice(randomNumberIndex, 1).toString();
+      shuffleArray.push(sliceValue);
+    }
+
+    console.log(shuffleArray);
+  };
 
   const setCurrentTitme = (nowTime: number) => {
     currentTime.value = nowTime;
@@ -38,5 +68,7 @@ export const useMusicControllerStore = defineStore("play", () => {
     setCurrentTitme,
     changePauseState,
     updateTime,
+    shufflePlaylist,
+    inputPlayList,
   };
 });
