f432bf5f72fd056aade39b12287d698eb984cf9d
qwer1234 <qwer1234@jinjoosoft.com>
2023-04-11 19:23:36 +0900

    playlist를 id값만 뽑는 computed 추가

--- src/stores/indexedDB.ts
+++ src/stores/indexedDB.ts
@@ -1,13 +1,23 @@
-import { ref } from "vue";
+import { computed, ref } from "vue";
 import { defineStore } from "pinia";
 import DataBase from "@/indexedDB/index";
+import type { DbField } from "@/components/icons/Heart.vue";
 
 export const useIndexedDBStore = defineStore("indexedDB", () => {
-  const favSongList = ref();
+  const favSongList = ref<DbField[]>([]);
   const webDB = DataBase.getInstance();
 
+  const getPlayList = computed(async () => {
+    await updateFavList;
+    const songList = Object.values(favSongList.value).map(
+      (value: DbField) => value.videoId
+    );
+
+    return songList;
+  });
+
   async function updateFavList() {
-    favSongList.value = await webDB.getFavList();
+    favSongList.value = (await webDB.getFavList()) as DbField[];
   }
 
   async function addSong(videoId: string, title: string) {
@@ -18,5 +28,5 @@ export const useIndexedDBStore = defineStore("indexedDB", () => {
   async function removeSong(videoId: string) {
     webDB.deleteData(videoId);
   }
-  return { updateFavList, removeSong, addSong, favSongList };
+  return { updateFavList, removeSong, addSong, favSongList, getPlayList };
 });
