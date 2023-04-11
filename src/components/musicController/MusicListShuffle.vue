4524de180715b3d709595a7abaceb0a7d9de027c
qwer1234 <qwer1234@jinjoosoft.com>
2023-04-11 19:23:06 +0900

    test 셔플기능, input 기능 추가

---
+++ src/components/musicController/MusicListShuffle.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <button @click="store.inputPlayList">input test</button>
+    <br />
+    <button @click="store.shufflePlaylist">shuffle test</button>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { useMusicControllerStore } from "@/stores/musicController";
+const store = useMusicControllerStore();
+</script>
+
+<style scoped lang="scss"></style>
