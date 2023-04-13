0f788c589c263d007735d67c3f187a88061f3736
qwer1234 <qwer1234@jinjoosoft.com>
2023-04-13 20:19:58 +0900

    player 컴포넌트 추가

---
+++ src/components/player/PlayerArea.vue
@@ -0,0 +1,95 @@
+<template>
+  <div class="playerArea">
+    <div class="player">
+      <img :src="thumbnailURL" class="" />
+
+      <div class="marquee">
+        <div class="title">
+          <span>{{ title }}</span>
+        </div>
+      </div>
+      <AppProgressBar type="video"></AppProgressBar>
+      <AppProgressBar type="sound"></AppProgressBar>
+    </div>
+    <div class="playList">노래 리스트</div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { computed, onMounted, ref } from "vue";
+import AppProgressBar from "../common/AppProgressBar.vue";
+import { useMusicControllerStore } from "@/stores/musicController";
+import { storeToRefs } from "pinia";
+import { useIndexedDBStore } from "@/stores/indexedDB";
+
+const dbStore = useIndexedDBStore();
+
+const MusicStore = useMusicControllerStore();
+const { currentIndex, playList } = storeToRefs(MusicStore);
+const title = ref("");
+
+const thumbnailURL = computed(
+  () =>
+    `https://i.ytimg.com/vi/${playList.value[currentIndex.value]}/hqdefault.jpg`
+);
+
+onMounted(async () => {
+  title.value = await dbStore.getPlaylistTitle(
+    playList.value[currentIndex.value]
+  );
+});
+</script>
+
+<style scoped lang="scss">
+.playerArea {
+  gap: 2%;
+  display: flex;
+  width: 70rem;
+  margin: 0 auto;
+  height: 30rem;
+
+  .player {
+    width: 49%;
+    border: 1px solid black;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding: 0 7rem;
+    box-sizing: border-box;
+
+    img {
+      width: 100%;
+    }
+    .marquee {
+      position: relative;
+      width: 100%;
+      height: 100%;
+      overflow: hidden;
+      .title {
+        position: absolute;
+        animation: textLoop 5s linear infinite;
+
+        span {
+          font-weight: 700;
+          font-size: x-large;
+          white-space: nowrap;
+        }
+      }
+    }
+  }
+  .playList {
+    width: 49%;
+    overflow-y: scroll;
+    border: 1px solid black;
+  }
+}
+
+@keyframes textLoop {
+  0% {
+    transform: translate3d(0, 0, 0);
+  }
+  100% {
+    transform: translate3d(-100%, 0, 0);
+  }
+}
+</style>
