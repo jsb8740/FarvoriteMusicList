b5241c699919caece22895e1222f0822d2f328eb
qwer1234 <qwer1234@jinjoosoft.com>
2023-04-14 18:12:16 +0900

    progress태그 input range 테스트 컴포넌트

---
+++ src/components/common/test.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <progress id="file" max="100" value="70" @click="test"></progress>
+    <input type="range" name="" id="" />
+  </div>
+</template>
+
+<script setup lang="ts">
+const test = (e: any) => {
+  console.log(e.offsetX);
+};
+</script>
+
+<style scoped>
+progress {
+  border: 1px solid red;
+}
+</style>
