d77c6040536d7ed5701a6b2bde47193b90d3a531
qwer1234 <qwer1234@jinjoosoft.com>
2023-04-14 18:12:48 +0900

    player랑 list 추가

--- src/components/player/PlayerArea.vue
+++ src/components/player/PlayerArea.vue
@@ -3,25 +3,54 @@
     <div class="player">
       <img :src="thumbnailURL" class="" />
 
-      <div class="marquee">
-        <div class="title">
-          <span>{{ title }}</span>
-          <span>{{ title }}</span>
+      <AppMarquee>
+        <template #default>
+          <div v-html="title"></div>
+        </template>
+      </AppMarquee>
+
+      <div class="musicController">
+        <div class="previous">
+          <PreviousIcon></PreviousIcon>
+        </div>
+
+        <div class="paused">
+          <PausedIcon></PausedIcon>
+        </div>
+
+        <div class="next">
+          <NextIcon></NextIcon>
         </div>
       </div>
+
+      <!-- 밑에 현재시간도 같이 출력해야할듯 -->
       <AppProgressBar type="video"></AppProgressBar>
+      <div>0:00 4:23</div>
+
       <AppProgressBar type="sound"></AppProgressBar>
+
+      <test></test>
     </div>
-    <div class="playList">노래 리스트</div>
+
+    <div class="playList">
+      <div class="">노래 리스트</div>
+      <PlayerList v-for="i in 3"></PlayerList>
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
-import { computed, onMounted, ref } from "vue";
+import { computed, onMounted, ref, watch } from "vue";
 import AppProgressBar from "../common/AppProgressBar.vue";
 import { useMusicControllerStore } from "@/stores/musicController";
 import { storeToRefs } from "pinia";
 import { useIndexedDBStore } from "@/stores/indexedDB";
+import AppMarquee from "../common/AppMarquee.vue";
+import PausedIcon from "@/components/icons/PausedIcon.vue";
+import test from "@/components/common/test.vue";
+import NextIcon from "../icons/NextIcon.vue";
+import PreviousIcon from "../icons/PreviousIcon.vue";
+import PlayerList from "@/components/player/PlayerList.vue";
 
 const dbStore = useIndexedDBStore();
 
@@ -34,11 +63,13 @@ const thumbnailURL = computed(
     `https://i.ytimg.com/vi/${playList.value[currentIndex.value]}/hqdefault.jpg`
 );
 
-onMounted(async () => {
+watch(thumbnailURL, async () => {
   title.value = await dbStore.getPlaylistTitle(
     playList.value[currentIndex.value]
   );
 });
+
+onMounted(async () => {});
 </script>
 
 <style scoped lang="scss">
@@ -47,7 +78,7 @@ onMounted(async () => {
   display: flex;
   width: 70rem;
   margin: 0 auto;
-  height: 30rem;
+  height: 40rem;
 
   .player {
     width: 49%;
@@ -58,34 +89,47 @@ onMounted(async () => {
     padding: 0 7rem;
     box-sizing: border-box;
 
+    .musicController {
+      display: flex;
+      align-items: center;
+      gap: 3.4rem;
+      margin-top: 0.5rem;
+      margin-bottom: 1rem;
+
+      .previous,
+      .next {
+        cursor: pointer;
+        padding: 0.3rem;
+        border-radius: 100%;
+
+        display: flex;
+        justify-content: center;
+        align-items: center;
+      }
+
+      .paused {
+        cursor: pointer;
+        background-color: white;
+        border-radius: 100%;
+        padding: 0.5rem 0.4rem 0.5rem 0.5rem;
+        box-shadow: 0 0 1rem rgba(0, 0, 0, 0.18);
+
+        display: flex;
+        justify-content: center;
+        align-items: center;
+      }
+    }
+
     img {
       width: 100%;
-    }
-    .marquee {
-      position: relative;
-      width: 100%;
-      height: 2.5rem;
-      overflow: hidden;
-      .title {
-        position: absolute;
-        animation: textLoop 7s linear infinite;
-        white-space: nowrap;
-        height: 2.5rem;
-        span {
-          font-weight: 700;
-          font-size: 1.5em;
-
-          white-space: nowrap;
-          float: left;
-          width: 100%;
-        }
-      }
     }
   }
   .playList {
     width: 49%;
     overflow-y: scroll;
     border: 1px solid black;
+    display: flex;
+    flex-direction: column;
   }
 }
 
