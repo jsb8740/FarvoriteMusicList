fa450ceeec97d2e5f49009e8d03a07c24fb3f064
qwer1234 <qwer1234@jinjoosoft.com>
2023-04-20 19:34:17 +0900

    css 추가 random버튼 추가

--- src/components/player/PlayerMusicList.vue
+++ src/components/player/PlayerMusicList.vue
@@ -1,24 +1,33 @@
 <template>
   <div class="playList">
-    <div class="top">노래 리스트</div>
-    <ol class="listItem">
+    <div class="header">
+      노래 리스트
+      <AppRandomButton @click="musicStore.shufflePlaylist"></AppRandomButton>
+    </div>
+    <ul class="listItem">
       <li v-for="item in props.musicList" :key="item">
         <PlayerListItem :video-id="item"> </PlayerListItem>
       </li>
-    </ol>
+    </ul>
   </div>
 </template>
 
 <script setup lang="ts">
 import PlayerListItem from "@/components/player/PlayerListItem.vue";
+import AppRandomButton from "@/components/common/AppRandomButton.vue";
+import { useMusicControllerStore } from "@/stores/musicController";
+
 export interface Props {
   musicList: string[];
 }
 
 const props = defineProps<Props>();
+
+const musicStore = useMusicControllerStore();
 </script>
 
 <style scoped lang="scss">
+@import "@/assets/color.scss";
 .playList {
   width: 49%;
   height: 100%;
@@ -26,25 +35,18 @@ const props = defineProps<Props>();
   display: flex;
   flex-direction: column;
 
-  .top {
+  .header {
     padding: 1.2rem 0 1.2rem 1.2rem;
     font-weight: 700;
     font-size: 1.6rem;
-    background-color: rgb(214, 212, 219);
-  }
-
-  ol {
-    li {
-      &:nth-child(odd) {
-      }
-      &:nth-child(even) {
-      }
-    }
-    margin: 0;
-    overflow-y: scroll;
+    background-color: $yellowPastel;
+    display: flex;
   }
 
   .listItem {
+    overflow-y: scroll;
+    margin: 0;
+    list-style: none;
   }
 }
 </style>
