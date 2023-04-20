0a680171818a670df3bd10e5f424e158dcb21802
qwer1234 <qwer1234@jinjoosoft.com>
2023-04-20 19:33:44 +0900

    new page 추가

---
+++ src/views/PlayerView.vue
@@ -0,0 +1,17 @@
+<template>
+  <PlayerArea :type="typeComputed"></PlayerArea>
+</template>
+
+<script setup lang="ts">
+import PlayerArea from "@/components/player/PlayerArea.vue";
+import { computed } from "vue";
+import { useRoute } from "vue-router";
+
+const route = useRoute();
+
+const typeComputed = computed(() =>
+  route.params.type === "favorites" ? `${route.params.type}` : "playlist"
+);
+</script>
+
+<style scoped></style>
