a7b16b92e610e8dfb172497236224c18c42f33b3
qwer1234 <qwer1234@jinjoosoft.com>
2023-04-25 19:29:14 +0900

    marquee 구조 변경

--- src/components/common/AppMarquee.vue
+++ src/components/common/AppMarquee.vue
@@ -1,64 +1,73 @@
 <template>
-  <div class="marquee">
-    <div>
-      <span>
-        <slot></slot>
-      </span>
-      <span>
-        <slot></slot>
-      </span>
+  <div class="wrap">
+    <div :class="classCheck">
+      <slot></slot>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
+import { computed } from "vue";
+
+export interface Props {
+  isPlay?: string;
+  stop?: string;
+}
+const props = defineProps<Props>();
+const classCheck = computed(() => {
+  if (props.stop) {
+    return "";
+  }
+  if (props.isPlay) {
+    return "marqueePlaying";
+  } else {
+    return "marqueeList";
+  }
+});
 // props가 필요할듯 list용인지  main용인지
 // class도 2개나눠서 list용 -> 글자가 width를 넘어가는 경우 hover시에는 marquee 실행
 // 다른 main용 클래스
 </script>
 
 <style scoped lang="scss">
-@use "sass:math";
-$divWidth: 700%;
-$marqueeHeight: 1.6rem;
-.marquee {
-  height: $marqueeHeight + 0.5;
+.wrap {
   width: 100%;
   overflow: hidden;
-  position: relative;
-  margin: 2.5rem 0;
-
-  div {
-    display: block;
-    width: $divWidth;
-    height: 100%;
-
-    position: absolute;
-    overflow: hidden;
-
-    animation: marquee 10s linear infinite;
-
-    span {
-      // float: left;
-      // span을 50%로 div의 반반나눔
-      display: inline-block;
-      width: 50%;
-
-      font-weight: 700;
-      font-size: $marqueeHeight;
-      line-height: $marqueeHeight;
+  .marqueePlaying {
+    animation: marqueePlaying 6s linear infinite;
+    display: inline-block;
+    white-space: nowrap;
+    animation-play-state: running;
+    &:hover {
+      animation-play-state: paused;
     }
+    // &: focusout;
+  }
+  &:hover .marqueeList {
+    animation: marqueeList 6s linear infinite;
+    display: inline-block;
+    white-space: nowrap;
+    animation-play-state: running;
   }
 }
-
-@keyframes marquee {
+@keyframes marqueePlaying {
   0% {
-    left: 0;
+    margin-left: 100%;
+    transform: translateX(0%);
   }
   100% {
-    // left: -250%;
-
-    left: math.div($divWidth, -2);
+    margin-left: 0;
+    transform: translateX(-100%);
+  }
+}
+@keyframes marqueeList {
+  0% {
+    margin-left: 0%;
+    transform: translateX(0%);
+  }
+  100% {
+    margin-left: 0;
+    transform: translateX(-100%);
   }
 }
 </style>
