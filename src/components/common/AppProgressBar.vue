730d43ddd967e4308a4ce190877ad5fd7663861c
qwer1234 <qwer1234@jinjoosoft.com>
2023-04-28 19:51:40 +0900

    리팩토링

--- src/components/common/AppProgressBar.vue
+++ src/components/common/AppProgressBar.vue
@@ -1,101 +1,178 @@
 <template>
-  <input
+  <div>
+    <!-- <input
     type="range"
     min="0"
     max="100"
-    :value="type === 'sound' ? volume : currentTimePercent"
-    @input="onInputSlidereValue"
-    @wheel.prevent="onMouseWheel"
     ref="slider"
-  />
-  <!-- :style="inputStyle" -->
+    :value="sliderValue"
+    @input="onInput"
+    @wheel="onMouseWheel"
+    :style="initStyle"
+  /> -->
+    <input
+      type="range"
+      :min="min"
+      :max="max"
+      ref="slider"
+      v-model="progress"
+      @input="onInput"
+      @wheel="onMouseWheel"
+    />
+    {{ progress }}
+  </div>
 </template>
 
 <script setup lang="ts">
-import { useMusicControllerStore } from "@/stores/musicController";
-import { useSoundControllerStore } from "@/stores/soundController";
-import { storeToRefs } from "pinia";
 import { computed, onMounted, ref, watch } from "vue";
 
+const min = 0;
+const max = 100;
+
+type ProgressType = "sound" | "time";
+
 export interface Props {
-  type: string;
-  // typeValue: number;
+  modelValue?: number;
+  progressType: ProgressType;
 }
-const props = defineProps<Props>();
-const soundStore = useSoundControllerStore();
-const { volume } = storeToRefs(soundStore);
-const musicStore = useMusicControllerStore();
-const { currentTime, duration, currentTimePercent } = storeToRefs(musicStore);
+export interface Emits {
+  (e: "update:modelValue", value: Props["modelValue"]): void;
+  (e: "update:modelValue", value: number): void;
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  progressType: "sound",
+});
+const emit = defineEmits<Emits>();
+
 const slider = ref<HTMLElement | null>(null);
+const innerProgress = ref(0);
+const isModelValueNull = computed(() => props.modelValue == null);
 
-const inputStyle = computed(() =>
-  props.type === "sound"
-    ? `--ProgressPercent: ${volume.value}%`
-    : "--ProgressPercent: 0%"
-);
+const progress = computed({
+  get: () => {
+    if (isModelValueNull.value) {
+      console.log("df");
 
-const onInputSlidereValue = (event: Event) => {
-  const value = Number((event.target as HTMLInputElement).value);
-  if (props.type === "sound") {
-    soundStore.setVolume(value);
-  } else {
-    console.log(value);
-    musicStore.setCurrentTime(value, "percent");
-    // musicStore.setCurrentTimePercent(value);
+      return innerProgress.value;
+    }
+
+    return props.modelValue as number;
+  },
+  set: (value) => {
+    let temp = value;
+
+    if (typeof value === "string") {
+      temp = Number(value);
+    }
+
+    if (isModelValueNull.value) {
+      innerProgress.value = temp;
+    }
+    emit("update:modelValue", temp);
+  },
+});
+
+const onMouseWheel = ({ deltaY }: WheelEvent) => {
+  switch (props.progressType) {
+    case "sound":
+      if (deltaY > 0) {
+        progress.value = Math.max(min, progress.value - 5);
+      } else if (deltaY < 0) {
+        progress.value = Math.min(max, progress.value + 5);
+      }
+      break;
+
+    default:
+      break;
   }
 };
-const onMouseWheel = (e: WheelEvent) => {
-  if (props.type === "sound") {
-    soundStore.volumeUpDownHandler(e.deltaY);
-  }
-};
 
-const getProgressWidth = (value: number, min: number, max: number) => {
-  const widthPercent = ((value - min) / (max - min)) * 100;
-  return widthPercent;
+const onInput = (e: Event) => {
+  const value = Number((e.target as HTMLInputElement).value);
+  progress.value = value;
 };
 
 const setProgressCss = (width: number) => {
+  console.log(width);
   (slider.value as HTMLElement).style.setProperty(
     "--ProgressPercent",
     `${width}%`
   );
 };
 
+watch(
+  () => progress.value,
+  (newVal) => {
+    setProgressCss(newVal);
+  }
+);
+
 onMounted(() => {
-  soundStore.volumeInit();
-  if (props.type !== "sound") {
+  if (props.progressType !== "sound") {
     (slider.value as HTMLInputElement).defaultValue = "0";
   }
+  setProgressCss(
+    isModelValueNull.value ? innerProgress.value : (props.modelValue as number)
+  );
 });
 
-watch(currentTime, () => {
-  if (props.type !== "sound") {
-    const percent = Math.round((currentTime.value / duration.value) * 100);
-    // const extraWidth = (100 - percent) / 10;
-    musicStore.setCurrentTimePercent(percent);
-  }
-});
+// const musicStore = useMusicControllerStore();
+// const { currentTimePercent } = storeToRefs(musicStore);
+// const soundStore = useSoundControllerStore();
 
-watch(currentTimePercent, () => {
-  if (props.type !== "sound") {
-    setProgressCss(Math.ceil(currentTimePercent.value));
-  }
-});
+// const sliderValue = ref(modelValue);
+// const slider = ref<HTMLElement | null>(null);
 
-watch(volume, () => {
-  // console.log(props.typeValue);
-  if (volume.value === 0 && props.type === "sound") {
-    setProgressCss(0);
-  }
+// const initStyle = computed(() => `--ProgressPercent: ${modelValue}%`);
 
-  if (props.type === "sound") {
-    // const min = Number((slider.value as HTMLInputElement).min);
-    // const max = Number((slider.value as HTMLInputElement).max);
-    // const widthPercent = getProgressWidth(volume.value as number, min, max);
-    setProgressCss(volume.value);
-  }
-});
+// handler
+// const onInput = (e: Event) => {
+//   const value = Number((e.target as HTMLInputElement).value);
+//   sliderValue.value = value;
+// };
+
+// const onMouseWheel = (e: WheelEvent) => {
+//   if (progressType === "sound") {
+//     sliderValue.value = soundStore.volumeUpDownHandler(e.deltaY);
+//   }
+// };
+
+// onMounted
+// onMounted(() => {
+//   console.log(sliderValue.value);
+
+//   soundStore.volumeInit();
+//   if (progressType !== "sound") {
+//     // (slider.value as HTMLInputElement).defaultValue = "0";
+//   }
+// });
+
+// // watch
+// watch(currentTimePercent, (newModelValue, old) => {
+//   console.log("newModelValue", newModelValue);
+//   console.log("old", old);
+
+//   // if (slider.value && progressType !== "sound") {
+//   //   setProgressCss(newModelValue);
+//   // }
+// });
+// watch(sliderValue, (newSliderValue) => {
+//   console.log(newSliderValue);
+//   if (slider.value) {
+//     switch (progressType) {
+//       case "sound":
+//         soundStore.setVolume(newSliderValue);
+//         break;
+//       case "time":
+//         musicStore.setCurrentTimeClick(newSliderValue);
+//         break;
+//     }
+
+//     emit("update:modelValue", sliderValue.value);
+//     setProgressCss(newSliderValue);
+//   }
+// });
 </script>
 
 <style scoped lang="scss">
@@ -146,6 +223,7 @@ input[type="range"] {
     transform: translate(0, -50%);
     width: var(--thumbRadius);
     height: var(--thumbRadius);
+
     /* margin-top: calc((var(--trackHeight) - var(--thumbRadius)) / 2); */
     background: $orangeColor;
     border-radius: 999px;
@@ -154,10 +232,4 @@ input[type="range"] {
     z-index: 1;
   }
 }
-
-/* ::before element to replace the slider track */
-
-/* `::-webkit-slider-runnable-track` targets the track (background) of a range slider in chrome and safari browsers. */
-
-/* `::-moz-range-track` targets the track (background) of a range slider in Mozilla Firefox. */
 </style>
