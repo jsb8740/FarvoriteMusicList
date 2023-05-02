9632e1e4b5bd355da9b937b6ca9a90d7dab2d1bb
qwer1234 <qwer1234@jinjoosoft.com>
2023-05-02 21:24:38 +0900

    이미지 컴포넌트 분리

---
+++ src/components/player/PlayerSoundImg.vue
@@ -0,0 +1,38 @@
+<template>
+  <div @click="soundStore.soundMute">
+    <img v-show="soundMuteImg" src="@/assets/volumeMute-white.png" />
+    <img v-show="soundLowImg" src="@/assets/volumeLow-white.png" />
+    <img v-show="soundHighImg" src="@/assets/volumeHigh-white.png" />
+  </div>
+</template>
+
+<script setup lang="ts">
+import { useSoundControllerStore } from "@/stores/soundController";
+import { storeToRefs } from "pinia";
+import { computed } from "vue";
+const soundStore = useSoundControllerStore();
+const { volume } = storeToRefs(soundStore);
+// // sound img 컨트롤
+const soundMuteImg = computed<boolean>(() =>
+  volume.value === 0 ? true : false
+);
+const soundLowImg = computed<boolean>(() =>
+  volume.value > 0 && volume.value <= 50 ? true : false
+);
+const soundHighImg = computed<boolean>(() =>
+  volume.value > 50 && volume.value <= 100 ? true : false
+);
+</script>
+
+<style scoped lang="scss">
+div {
+  display: flex;
+  align-items: center;
+  img {
+    width: 1.1rem;
+    height: 1.1em;
+    margin: 0;
+    cursor: pointer;
+  }
+}
+</style>
