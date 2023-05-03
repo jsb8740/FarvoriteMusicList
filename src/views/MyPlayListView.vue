8d4b0eee9d80473a98f7472408f90b9064e9d674
qwer1234 <qwer1234@jinjoosoft.com>
2023-05-03 22:37:22 +0900

    주석처리

--- src/views/MyPlayListView.vue
+++ src/views/MyPlayListView.vue
@@ -93,19 +93,7 @@
       <h1>저장한 플레이리스트</h1>
       <div>플레이리스트들</div>
     </div>
-
-    <!-- 컴포넌트화 해야할듯 -->
-    <!-- 왁타버스 디자인 참고 -->
-    <!-- iframe으로 display none -->
-    <!-- or 서버에 음악을 저장 흠.. -->
     <hr />
-
-    <!-- <PlayList>
-      <template #playListTitle>저장한 플레이리스트</template>
-      <div v-for="index in 4" :key="index">
-        <PlayListItem></PlayListItem>
-      </div>
-    </PlayList> -->
 
     <hr />
 
