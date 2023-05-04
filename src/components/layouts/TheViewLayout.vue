e17589d7a0c354fffe2b3d191db4cb7bab0e7c6b
qwer1234 <qwer1234@jinjoosoft.com>
2023-05-04 19:31:19 +0900

    css 변경

--- src/components/layouts/TheViewLayout.vue
+++ src/components/layouts/TheViewLayout.vue
@@ -1,9 +1,25 @@
 <template>
   <div class="contentLayout">
-    <slot></slot>
+    <h1>
+      <slot name="title"></slot>
+    </h1>
+    <hr />
+    <div>
+      <slot></slot>
+    </div>
   </div>
 </template>
 
 <script setup lang="ts"></script>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+h1 {
+  font-weight: 700px;
+  color: white;
+  margin-left: 3%;
+}
+
+hr {
+  margin: 1rem 3% 3rem 3%;
+}
+</style>
