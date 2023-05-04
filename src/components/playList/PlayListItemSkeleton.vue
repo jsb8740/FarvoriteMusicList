ecf2674ea9f9d4994ae2f9fb23b0efd8679d77f1
qwer1234 <qwer1234@jinjoosoft.com>
2023-05-04 19:30:20 +0900

    스켈레톤 ui 추가

---
+++ src/components/playList/PlayListItemSkeleton.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="skeletons">
+    <div class="thumbnail"></div>
+    <div class="text"></div>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped lang="scss">
+.skeletons {
+  margin: 0 3%;
+  display: inline-block;
+  width: 320px;
+  height: 210px;
+  .thumbnail {
+    width: 100%;
+    height: 180px;
+    background-color: $skeletonsColor;
+    border-radius: 1rem;
+    margin-bottom: 5px;
+  }
+  .text {
+    height: 25px;
+    width: 100%;
+    background-color: $skeletonsColor;
+    border-radius: 0.3rem;
+  }
+}
+</style>
