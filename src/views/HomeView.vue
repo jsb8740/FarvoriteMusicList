64711bc0fd3ca37c0a2fcf666c91b18eb8c3834e
qwer1234 <qwer1234@jinjoosoft.com>
2023-05-04 19:27:22 +0900

    css 변경

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,10 +1,13 @@
 <template>
   <TheViewLayout>
+    <template #title> Home </template>
     <template #default>
-      <div class="test">
+      <div class="contents">
         <PlayerArea></PlayerArea>
         <AppMarquee></AppMarquee>
       </div>
+
+      <PlayListItemSkeleton></PlayListItemSkeleton>
     </template>
   </TheViewLayout>
 </template>
@@ -13,9 +16,10 @@
 import TheViewLayout from "@/components/layouts/TheViewLayout.vue";
 import PlayerArea from "@/components/player/PlayerArea.vue";
 import AppMarquee from "@/components/common/AppMarquee.vue";
+import PlayListItemSkeleton from "@/components/playList/PlayListItemSkeleton.vue";
 </script>
 <style scoped lang="scss">
-.test {
+.contents {
   width: 100%;
 }
 </style>
